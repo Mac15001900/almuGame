3d345e90ee97644eb7d1f5a36c9e9e208502539d
Matheo573 <mateusz573@gmail.com>
2021-07-29 12:50:27 +0200

    taby - objects.js

--- objects.js
+++ objects.js
@@ -30,23 +30,22 @@ let ship = {
         if(asteroids.length > 1){
             for (let i = asteroids.length - 1; i >= 0; i--) {
                 if (circleCollide(ship, asteroids[0])){
-              
                 }
             }
         }
     },
 
     render: function(){
-    newX = 0;
-    if (0 < this.x < 1920 || 0 < this.y < 1080) {
-      this.x = (this.x + 1920) % 1920;
-      this.y = (this.y + 1080) % 1080;
-    }//width=1920 height=1080
-    drawRotatedRect(this, this.angle);//LOREM IPSUM
-  },
+        newX = 0;
+        if (0 < this.x < 1920 || 0 < this.y < 1080) {
+            this.x = (this.x + 1920) % 1920;
+            this.y = (this.y + 1080) % 1080;
+        }//width=1920 height=1080
+        drawRotatedRect(this, this.angle);//LOREM IPSUM
+    },
 }
-  let cooldown = 0.3;
-  let helpcooldown = 0;
+let cooldown = 0.3;
+let helpcooldown = 0;
 let Missile = function(x, y, angle) {
 	this.basespeed= 500;
 	this.angle= angle-Pi/2;
@@ -54,68 +53,66 @@ let Missile = function(x, y, angle) {
 	this.y= y;
 	this.width= 12;
 	this.height= 30;
-  this.radius = 10;
+    this.radius = 10;
 	this.color= "#EB0018";
 	this.update = function(delta){
 		if(0<x<canvas.width && 0<y<canvas.height){
 			this.y += ((this.basespeed + ship.speed) * Math.sin(this.angle)) * delta;
 			this.x += ((this.basespeed + ship.speed) * Math.cos(this.angle)) * delta;
 		}
-
-		
 	};
-  this.render = function(){
-    drawCircle(this)
-  };
-  this.forDeletion = function(){
-  	return(!(0<this.x && this.x<canvas.width && 0<this.y && this.y<canvas.height));
-  };
+    this.render = function(){
+        drawCircle(this)
+    };
+    this.forDeletion = function(){
+        return(!(0<this.x && this.x<canvas.width && 0<this.y && this.y<canvas.height));
+    };
 }
 
 let Asteroid = function() {
-this.time = 0;
-this.color = "#dfff20";
-this.random = Math.ceil(Math.random()*4);
-this.x = 0;
-this.y = 0;
-this.rad = 200;
-this.speedX = 0;
-this.speedY = 0;
-this.size = 100 + Math.random()*150;
-  if( this.random === 1){
-    this.x = -200;
-    this.y = canvas.height * Math.random();
-    this.speedX = 50 + Math.random()*50;
-    this.speedY = Math.random()*50 - Math.random()*50;
-  }
-  else if( this.random === 2){
-    this.x = canvas.width * Math.random();
-    this.y = -200;
-    this.speedX = Math.random()*50 - Math.random()*50;
-    this.speedY = 50 + Math.random()*50;
-  }
-  else if( this.random === 3){
-    this.x = canvas.width + 200;
-    this.y = canvas.height * Math.random();
-    this.speedX = -50 - Math.random()*50;
-    this.speedY = Math.random()*50 - Math.random()*50;
-  }
-  else if( this.random === 4){
-    this.x = canvas.width * Math.random();
-    this.y = canvas.height + 200;
-    this.speedX = Math.random()*50 - Math.random()*50;
-    this.speedY = -50 - Math.random()*50;
-  }
+    this.time = 0;
+    this.color = "#dfff20";
+    this.random = Math.ceil(Math.random()*4);
+    this.x = 0;
+    this.y = 0;
+    this.rad = 200;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.size = 100 + Math.random()*150;
+    if( this.random === 1){
+        this.x = -200;
+        this.y = canvas.height * Math.random();
+        this.speedX = 50 + Math.random()*50;
+        this.speedY = Math.random()*50 - Math.random()*50;
+    }
+    else if( this.random === 2){
+        this.x = canvas.width * Math.random();
+        this.y = -200;
+        this.speedX = Math.random()*50 - Math.random()*50;
+        this.speedY = 50 + Math.random()*50;
+    }
+    else if( this.random === 3){
+        this.x = canvas.width + 200;
+        this.y = canvas.height * Math.random();
+        this.speedX = -50 - Math.random()*50;
+        this.speedY = Math.random()*50 - Math.random()*50;
+    }
+    else if( this.random === 4){
+        this.x = canvas.width * Math.random();
+        this.y = canvas.height + 200;
+        this.speedX = Math.random()*50 - Math.random()*50;
+        this.speedY = -50 - Math.random()*50;
+    }
 
-  this.update = function(delta){
-      this.x += this.speedX*delta;
-      this.y += this.speedY*delta;
-  };
-  this.render = function(){
-    context.fillStyle = this.color;
-    context.fillRect( this.x, this.y, this.size, this.size);
-  };
-  this.forDeletion = function(bool) {
-    return(bool);
-  } 
+    this.update = function(delta){
+        this.x += this.speedX*delta;
+        this.y += this.speedY*delta;
+    };
+    this.render = function(){
+        context.fillStyle = this.color;
+        context.fillRect( this.x, this.y, this.size, this.size);
+    };
+    this.forDeletion = function(bool) {
+        return(bool);
+    } 
 }
